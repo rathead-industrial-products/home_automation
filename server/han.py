c0e97a9d1d8fd595fa3d603c1a07e531b5aa8021
Jeff Mitchell <mitchell@mindmentum.com>
2020-12-16 10:01:05 -0800

    debugging...

--- server/han.py
+++ server/han.py
@@ -489,10 +489,10 @@ class healthThread(threading.Thread):
         server_log.info("healthThread running")
 
         while True:
-            health_status = { 'host' : self.host_name }  # dictionary of health related parameters
+            health_status = { 'host' : host_name }  # dictionary of health related parameters
 
             # post health to remote server
-            requests.post(self.REMOTE_URL, data=health_status)
+            requests.post(self.REMOTE_URL, json=health_status)
 
             # report health to magic mirror
             msg = ("HEALTH_NOTICE", health_status)  # message must be a list
